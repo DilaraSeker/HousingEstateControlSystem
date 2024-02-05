d2e4fce659a86585d753ff3abfcd511e6b3daa07
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-05 23:08:30 +0300

    Create User and Condo models

---
+++ HousingEstateControlSystem.Repositories/Models/Condo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HousingEstateControlSystem.Repositories.Models
+{
+    public class Condo
+    {
+        public int CondoId { get; set; }
+        public string Block { get; set; }
+        public bool IsOccupied { get; set; }
+        public string Type { get; set; }
+        public int Floor { get; set; }
+        public int Number { get; set; }
+        public bool Owner { get; set; } // false if it is rented
+    }
+}
---
+++ HousingEstateControlSystem.Repositories/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HousingEstateControlSystem.Repositories.Models
+{
+    public class User
+    {
+        public int UserId { get; set; } = default!;
+        public string FullName { get; set; } = default!;
+        public string TCNo { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+    }
+}
