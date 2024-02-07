a9096649dd0bb440fe48e699816de5375fc115ed
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-08 02:06:11 +0300

    Add Bill and Dues models

---
+++ HousingEstateControlSystem.Repositories/Models/Bill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HousingEstateControlSystem.Repositories.Models
+{
+    public class Bill
+    {
+        public int BillId { get; set; }
+        public int CondoId { get; set; } // Foreign key for Condo
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime MonthYear { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
---
+++ HousingEstateControlSystem.Repositories/Models/Dues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingEstateControlSystem.Repositories.Models
+{
+    public class Dues
+    {
+        public int DuesId { get; set; }
+        public int CondoId { get; set; } // Foreign key for Condo
+        public decimal Amount { get; set; }
+        public DateTime MonthYear { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}
