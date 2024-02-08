f621666ad3452e425c18b7476d5ee0d59921896a
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-08 23:13:57 +0300

    Add UserRepository

---
+++ HousingEstateControlSystem.Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using HousingEstateControlSystem.Repositories.Interfaces;
+using HousingEstateControlSystem.Repositories.Models;
+
+namespace HousingEstateControlSystem.Repositories.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public UserRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public User GetUserById(int userId)
+        {
+            return _context.Users.FirstOrDefault(u => u.UserId == userId);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public void AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void UpdateUser(User user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+        public void DeleteUser(int userId)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserId == userId);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
