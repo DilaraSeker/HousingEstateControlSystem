bd57b844b273b69b1f22f3b9934be75536a7eae3
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-08 23:20:39 +0300

    Add BillRepository

---
+++ HousingEstateControlSystem.Repositories/Implementations/BillRepository.cs
@@ -0,0 +1,47 @@
+﻿using HousingEstateControlSystem.Repositories.Interfaces;
+using HousingEstateControlSystem.Repositories.Models;
+
+namespace HousingEstateControlSystem.Repositories.Implementations
+{
+    public class BillRepository : IBillRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public BillRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public Bill GetBillById(int billId)
+        {
+            return _context.Bills.FirstOrDefault(b => b.BillId == billId);
+        }
+
+        public List<Bill> GetAllBills()
+        {
+            return _context.Bills.ToList();
+        }
+
+        public void AddBill(Bill bill)
+        {
+            _context.Bills.Add(bill);
+            _context.SaveChanges();
+        }
+
+        public void UpdateBill(Bill bill)
+        {
+            _context.Bills.Update(bill);
+            _context.SaveChanges();
+        }
+
+        public void DeleteBill(int billId)
+        {
+            var bill = _context.Bills.FirstOrDefault(b => b.BillId == billId);
+            if (bill != null)
+            {
+                _context.Bills.Remove(bill);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
