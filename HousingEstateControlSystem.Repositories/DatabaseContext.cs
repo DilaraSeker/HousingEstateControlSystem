55763eaf156de2de218eadca5982f814fa2dbe5b
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-08 03:03:46 +0300

    Add Database Context without method body

---
+++ HousingEstateControlSystem.Repositories/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HousingEstateControlSystem.Repositories.Models;
+
+namespace HousingEstateControlSystem.Repositories
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Condo> Condos { get; set; }
+        public DbSet<Dues> Dues { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
