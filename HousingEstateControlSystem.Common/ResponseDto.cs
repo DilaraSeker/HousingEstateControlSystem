0b27f61cd9819983f6a7f497c0d415ba32160aee
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-10 02:41:23 +0300

    Add Status Code to ResponseDTO

--- HousingEstateControlSystem.Common/ResponseDto.cs
+++ HousingEstateControlSystem.Common/ResponseDto.cs
@@ -5,18 +5,21 @@ namespace HousingEstateControlSystem.Common
     {
         public T Data { get; set; }
         public List<string> Errors { get; set; }
+        public int StatusCode { get; set; }
         private ResponseDto() { }
 
         public ResponseDto(T data)
         {
             Data = data;
             Errors = new List<string>();
+            StatusCode = 200;
         }
 
         public ResponseDto(List<string> errors)
         {
             Data = default(T);
             Errors = errors;
+            StatusCode = 400;
         }
 
         public static ResponseDto<T> Success(T data)
