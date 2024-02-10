d862ab8827d193c1fd1dbe986c49f1227f056d44
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-10 23:35:57 +0300

    Add Middleware

---
+++ HousingEstateControlSystem.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace HousingEstateControlSystem.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+            var result = JsonConvert.SerializeObject(new { error = exception.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
