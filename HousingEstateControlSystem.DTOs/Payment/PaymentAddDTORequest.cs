b4d0d695a5e54d5840b3429f35a67839e9b21643
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-11 00:59:26 +0300

    Add Payment DTOs

---
+++ HousingEstateControlSystem.DTOs/Payment/PaymentAddDTORequest.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HousingEstateControlSystem.DTOs
+{
+    public class PaymentAddDTORequest
+    {
+        public int UserId { get; set; }
+        public int CondoId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentType { get; set; } // Aidat/Fatura(Elektrik-Su-Doğalgaz)
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
---
+++ HousingEstateControlSystem.DTOs/Payment/PaymentUpdateDTORequest.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HousingEstateControlSystem.DTOs
+{
+    public class PaymentUpdateDTORequest
+    {
+        public int PaymentId { get; set; }
+        public int UserId { get; set; }
+        public int CondoId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentType { get; set; } // Aidat/Fatura(Elektrik-Su-Doğalgaz)
+        public int Year { get; set; }
+        public int Month { get; set; }
+    }
+}
