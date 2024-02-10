2d6d8f2c28b4cfffc15fa50ade606f3e531e50a0
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-11 01:00:10 +0300

    Add PaymentService

---
+++ HousingEstateControlSystem.Services/Implementations/PaymentService .cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using HousingEstateControlSystem.DTOs;
+using HousingEstateControlSystem.Repositories.Models;
+using HousingEstateControlSystem.Services.Interfaces;
+using HousingEstateControlSystem.Repositories;
+
+namespace HousingEstateControlSystem.Services.Implementations
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IPaymentRepository _paymentRepository;
+        private readonly IMapper _mapper;
+
+        public PaymentService(IPaymentRepository paymentRepository, IMapper mapper)
+        {
+            _paymentRepository = paymentRepository;
+            _mapper = mapper;
+        }
+
+        public PaymentDTO AddPayment(PaymentAddDTORequest paymentAddDTO)
+        {
+            var payment = _mapper.Map<Payment>(paymentAddDTO);
+            payment.PaymentDate = DateTime.UtcNow;
+            var addedPayment = _paymentRepository.AddPayment(payment);
+            return _mapper.Map<PaymentDTO>(addedPayment);
+        }
+
+        public void DeletePayment(int paymentId)
+        {
+            _paymentRepository.DeletePayment(paymentId);
+        }
+
+        public List<PaymentDTO> GetAllPayments()
+        {
+            var payments = _paymentRepository.GetAllPayments();
+            return _mapper.Map<List<PaymentDTO>>(payments);
+        }
+
+        public PaymentDTO GetPaymentById(int paymentId)
+        {
+            var payment = _paymentRepository.GetPaymentById(paymentId);
+            return _mapper.Map<PaymentDTO>(payment);
+        }
+
+        public void UpdatePayment(PaymentUpdateDTORequest paymentUpdateDTO)
+        {
+            var existingPayment = _paymentRepository.GetPaymentById(paymentUpdateDTO.PaymentId);
+            if (existingPayment == null)
+            {
+                throw new ApplicationException("Payment not found");
+            }
+
+            _mapper.Map(paymentUpdateDTO, existingPayment);
+            _paymentRepository.UpdatePayment(existingPayment);
+        }
+    }
+}
---
+++ HousingEstateControlSystem.Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,13 @@
+﻿using HousingEstateControlSystem.DTOs;
+
+namespace HousingEstateControlSystem.Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        PaymentDTO GetPaymentById(int paymentId);
+        List<PaymentDTO> GetAllPayments();
+        PaymentDTO AddPayment(PaymentAddDTORequest paymentAddDTO);
+        void UpdatePayment(PaymentUpdateDTORequest paymentUpdateDTO);
+        void DeletePayment(int paymentId);
+    }
+}
