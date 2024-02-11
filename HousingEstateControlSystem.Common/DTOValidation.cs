4a99bf3f4c2853081a5c2bc342a62e8951ebe00f
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-11 21:55:12 +0300

    Add DTOValidation

---
+++ HousingEstateControlSystem.Common/DTOValidation.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace HousingEstateControlSystem.Common
+{
+    public static class DTOValidation
+    {
+        public static bool ValidateDTO<T>(T dto, out List<ValidationResult> results)
+        {
+            var validationContext = new ValidationContext(dto, serviceProvider: null, items: null);
+            results = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, results, validateAllProperties: true);
+        }
+    }
+}
