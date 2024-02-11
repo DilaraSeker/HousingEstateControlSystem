6802ecb3bfb4a89384a3483a72ed3e24726b3f99
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-11 06:07:25 +0300

    Edit property names

--- HousingEstateControlSystem.DTOs/User/UserAddDtoRequest.cs
+++ HousingEstateControlSystem.DTOs/User/UserAddDtoRequest.cs
@@ -2,9 +2,10 @@
 {
     public class UserAddDtoRequest
     {
-        public string Name { get; set; }
+        public string FullName { get; set; }
         public string TCNo { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
+        public string PhoneNumber { get; set; }
+        public int? CondoId { get; set; }
     }
 }
--- HousingEstateControlSystem.DTOs/User/UserDTO.cs
+++ HousingEstateControlSystem.DTOs/User/UserDTO.cs
@@ -7,5 +7,6 @@
         public string TCNo { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public int? CondoId { get; set; }
     }
 }
