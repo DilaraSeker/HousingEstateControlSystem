003db407c35ff88c155ea61d088bfd5e52d8acad
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-11 22:16:32 +0300

    Add Auth Controller

---
+++ HousingEstateControlSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using HousingEstateControlSystem.DTOs.Auth;
+using HousingEstateControlSystem.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousingEstateControlSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDTO loginDto)
+        {
+            var result = _authService.Login(loginDto);
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterDTO registerDto)
+        {
+            var result = _authService.Register(registerDto);
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+    }
+}
