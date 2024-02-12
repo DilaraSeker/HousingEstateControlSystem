fff582e661494a4b8a4d44912aedaf393e2c1592
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-12 03:44:19 +0300

    Add UserRoleRepository

---
+++ HousingEstateControlSystem.Repositories/Implementations/UserRoleRepository.cs
@@ -0,0 +1,41 @@
+﻿using HousingEstateControlSystem.Repositories.Interfaces;
+using HousingEstateControlSystem.Repositories.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HousingEstateControlSystem.Repositories.Implementations
+{
+    public class UserRoleRepository : IUserRoleRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public UserRoleRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void AddUserRole(UserRole userRole)
+        {
+            _context.UserRoles.Add(userRole); // Kullanıcı rolünü ekliyoruz
+            _context.SaveChanges(); // Değişiklikleri kaydediyoruz
+        }
+
+        public void RemoveUserRole(string userId, string roleId)
+        {
+            var userRoleToRemove = _context.UserRoles.FirstOrDefault(ur => ur.UserId == userId && ur.RoleId == roleId);
+            if (userRoleToRemove != null)
+            {
+                _context.UserRoles.Remove(userRoleToRemove);
+                _context.SaveChanges();
+            }
+        }
+
+        public List<string> GetUserRoles(string userId)
+        {
+            var roleIds = _context.UserRoles.Where(ur => ur.UserId == userId).Select(ur => ur.RoleId).ToList();
+            return roleIds;
+        }
+    }
+}
---
+++ HousingEstateControlSystem.Repositories/Interfaces/IUserRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using HousingEstateControlSystem.Repositories.Models;
+using System.Collections.Generic;
+
+namespace HousingEstateControlSystem.Repositories.Interfaces
+{
+    public interface IUserRoleRepository
+    {
+        void AddUserRole(UserRole userRole);
+        void RemoveUserRole(string userId, string roleId);
+        List<string> GetUserRoles(string userId);
+    }
+}
