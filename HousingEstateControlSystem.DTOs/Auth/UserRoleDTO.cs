b4d74bc7209d2adab6e06899793a4438d255e523
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-12 03:43:54 +0300

    Add UserRoleDTO

---
+++ HousingEstateControlSystem.DTOs/Auth/UserRoleDTO.cs
@@ -0,0 +1,9 @@
+﻿using HousingEstateControlSystem.Repositories.Models;
+
+namespace HousingEstateControlSystem.DTOs.Auth
+{
+    public class UserRoleDTO
+    {
+        public Role Role { get; set; }
+    }
+}
